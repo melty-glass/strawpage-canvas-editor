ab59405e1ce8c2787aa51bc75ef9a441c51ee5a4
melty-glass <109243017+melty-glass@users.noreply.github.com>
2024-08-01 07:38:46 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+pic = document.querySelector('.picasso');
+width = pic.firstChild.getAttribute('width');
+height = pic.firstChild.getAttribute('height');
+
+resize = (w, h) => {
+    pic.parentElement.parentElement.setAttribute('style', width: ${w + 4}px; height: ${h + 47.6}px);
+    pic.firstChild.setAttribute('width', w+'px');
+    pic.firstChild.setAttribute('height', h+'px');
+
+    width = w;
+    height = h;
+};
+
+ref = (url, dx = 0, dy = 0, dWidth = width, dHeight = height) => {
+    const c = pic.firstChild.getContext('2d');
+
+    const img = new Image();
+    img.src = url;
+
+    img.onload = () => {
+        c.drawImage(img, dx, dy, dWidth, dHeight);
+    }
+}
